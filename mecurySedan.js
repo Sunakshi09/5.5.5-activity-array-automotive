5a1434c71e0b9640e527c393aab2d43bb4d9a9b4
sunaks <sunakshithapar@gmail.com>
2023-02-05 23:33:29 -0500

    strong commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,71 +1,157 @@
-//this includes the vehicle class as a module
-import { Vehicle } from "./vehicle.js";
-const VehicleModule = require("./vehicleBaseClass")
+class Vehicle {
+  constructor(make, model, year, color, mileage) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.passenger = 0;
+    this.speed = 0;
+    this.mileage = mileage;
+    this.started = false;
+    this.numberOfWheels = 0;
+  }
 
-//this shows how to call from this module...
-
-class Car extends VehicleModule {
-    constructor(make, model, year, color, mileage) {
-    super(make, model, year, color, mileage);
-        this.maxPassengers = 5;
-        this.passenger = 0;
-        this.numberOfWheels = 4;
-        this.maxSpeed = 160;
-        this.fuel = 10;
-        this.scheduleService = false;
-    }
-}
-    
-checkService(); {
-         
-        if (this.mileage > 30000) {            
-        this.scheduleService = true
-        return this.scheduleService;                       
-    }
-}
-
-start(); {
-    
+  start() {
     if (this.fuel > 0) {
-        console.log("engine started...!!!");
-        return this.started = true;
+      console.log("engine started...!!!");
+      return (this.started = true);
     } else {
-        console.log("engine cannot start...");
-        return this.started = false;
+      console.log("engine cannot start...");
+      return (this.started = false);
     }
-}
-
-loadPassenger(num); {
-    if (this.passenger < this.maxPassengers) {
-        if ((num + this.passenger) <= this.maxPassengers) {
-            this.passenger = num;
-            return this.passenger;               
+  }
+  accelerate() {
+    if (this.started) {
+      if (this.fuel > 0) {
+        console.log((this.speed += 1));
+        this.fuel = this.fuel - 1;
+      } else {
+        console.log("out of fuel.");
+        this.stop();
+      }
+    } else {
+      alert("You need to start the engine first.");
+    }
+  }
+  decelerate() {
+    if (this.started) {
+      if (this.fuel > 0) {
+        if (this.speed > 0) {
+          console.log((this.speed -= 1));
+          this.fuel = this.fuel - 1;
         } else {
-            console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
-
+          console.log(this + " has stopped moving");
+          this.fuel = this.fuel - 1;
         }
+      } else {
+        console.log("out of fuel.");
+        this.stop();
+      }
     } else {
-        console.log(this.model + " " + this.make + " is full");
+      alert("You need to start the engine first.");
     }
+  }
+  stop() {
+    this.started = false;
+  }
+
+  /*drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }*/
+
+  typeOfVehicle(wheels) {
+    if (this.numberOfWheels == 8 && 8 == wheels) {
+      console.log(this.model + " " + this.make + " is a Truck");
+    } else if (this.numberOfWheels == 4 && 4 == wheels) {
+      console.log(this.model + " " + this.make + " is a CAr");
+    } else if (this.numberOfWheels == 2 && 2 == wheels) {
+      console.log(this.model + " " + this.make + " is a Bike");
+    } else {
+      console.log("Unknown type of vehicle");
+    }
+  }
 }
 
+class Car extends Vehicle {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+    this.maxPassengers = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maxSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
 
-let myCar = new Car('mercury', 'sable_sedan', '2009', 'silver', 57832)
+  checkService() {
+    if (this.mileage > 30000) {
+      this.scheduleService = true;
+      return this.scheduleService;
+    }
+  }
 
-myCar.start()
-myCar.loadPassenger(5)
-myCar.stop()
-myCar.checkService()
+  start() {
+    if (this.fuel > 0) {
+      console.log("engine started...!!!");
+      return (this.started = true);
+    } else {
+      console.log("engine cannot start...");
+      return (this.started = false);
+    }
+  }
 
-console.log(myCar)
+  loadPassenger(num) {
+    if (this.passenger < this.maxPassengers) {
+      if (num + this.passenger <= this.maxPassengers) {
+        this.passenger = num;
+        return this.passenger;
+      } else {
+        console.log(
+          this.model +
+            " " +
+            this.make +
+            " not have enough space to take all passengers."
+        );
+      }
+    } else {
+      console.log(this.model + " " + this.make + " is full");
+    }
+  }
+}
+
+let myCar = new Car("mercury", "sable_sedan", "2009", "silver", 57832);
+
+myCar.start();
+myCar.loadPassenger(5);
+myCar.stop();
+myCar.checkService();
+
+console.log(myCar);
 
 
 
-let mytruck = new truck('ford', 'maverick', '2022', 'oxford white', 40000)
- 
-mytruck.start()
-mytruck.loadPassenger(5)
-mytruck.stop()
-mytruck.checkService()
 
-console.log(mytruck)
+
+
+
+let myTruck = new Truck("ford", "maverick", "2022", "oxford white", 40000);
+
+myTruck.start();
+myTruck.loadPassenger(5);
+myTruck.stop();
+myTruck.checkService();
+
+console.log(myTruck);
